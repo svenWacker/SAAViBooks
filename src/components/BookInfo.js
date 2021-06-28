0cd17f954aead1f014da68d1e10398a338b07454
viviane <v.buschwallace@yahoo.com>
2021-06-28 11:51:49 +0200

    book details

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -4,11 +4,27 @@ import { Link } from "react-router-dom";
 const BookInfo = () => {
   return (
     <React.Fragment>
-      <div className="book-details-container">
+      <div className="book-container">
         <div className="book-cover-img-container">
           <a href="/book-cover-link" target="_blank">
             <img className="book-cover-img" />
           </a>
+        </div>
+        <div className="book-details-container">
+          <a href="/book-title-link" target="_blank">
+            <h2 className="book-title">Book Title</h2>
+          </a>
+          <a href="/book-author-link" target="_blank">
+            <h3 className="book-author">by author name</h3>
+          </a>
+          <div className="book-small-details">
+            <span>published: year</span>
+            <span>Google rating: rating</span>
+            <span>Community rating: rating</span>
+            <span>Community ratings: number of ratings</span>
+          </div>
+          <div className="book-series">Books series with number</div>
+          <div className="book-lang">Edition Language: language</div>
         </div>
       </div>
       <div className="book-reviews-container"></div>
