ad0150777035381b9d97c2aed8f54ada3baea5e5
viviane <v.buschwallace@yahoo.com>
2021-06-28 09:57:39 +0200

    test

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const BookInfo = () => {
-  return <React.Fragment></React.Fragment>;
+  return <React.Fragment>c</React.Fragment>;
 };
 
 export default BookInfo;
