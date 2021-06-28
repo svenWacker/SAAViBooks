6575dca2857d65368910b12151110870c5d31363
viviane <v.buschwallace@yahoo.com>
2021-06-28 20:39:28 +0200

    border of book container

--- src/components/Books.js
+++ src/components/Books.js
@@ -9,7 +9,7 @@ const Books = () => {
     <React.Fragment>
       <div className="books-wrapper">
         <h2 className="page-headings">Books</h2>
-        <h3>Coming soon</h3>
+        <h3>Top Rated Books/Book Recommendations/Book Of The Day</h3>
       </div>
       <BookInfo />
     </React.Fragment>
