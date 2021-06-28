574920a323feb01a54f4b4480c2cedb6f91bb485
viviane <v.buschwallace@yahoo.com>
2021-06-28 11:23:22 +0200

    book cover

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -4,8 +4,14 @@ import { Link } from "react-router-dom";
 const BookInfo = () => {
   return (
     <React.Fragment>
-      <div className="book-details-wrapper"></div>
-      <div className="book-reviews-wrapper"></div>
+      <div className="book-details-container">
+        <div className="book-cover-img-container">
+          <a href="/book-cover-link" target="_blank">
+            <img className="book-cover-img" />
+          </a>
+        </div>
+      </div>
+      <div className="book-reviews-container"></div>
     </React.Fragment>
   );
 };
