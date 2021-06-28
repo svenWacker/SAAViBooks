71c9d257bd878b6e215d3d5496be56f3bd1b0066
svenWacker <s.u.wacker@outlook.de>
2021-06-28 15:33:02 +0200

    new main - adding URL

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
         <Switch>
           <Route path="/" exact component={Landing} />
           <Route path="/home" exact component={Landing} />
+          <Route path="/SAAViBooks/" exact component={Landing} />
           <Route path="/books" exact component={Books} />
           <Route path="/about" exact component={About} />
           <Route path={() => "/main" || "/admin" || "/any-other-word"}>
