ac6eb25dc63933f52870cbafc3b8c2caaef67cc1
viviane <v.buschwallace@yahoo.com>
2021-07-01 10:06:48 +0200

    merging main

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,25 +2,6 @@ import React, { useState } from "react";
 import request from "superagent";
 
 const Search = (props) => {
-<<<<<<< HEAD
-  const [userInput, setUserInput] = useState("");
-  const [booksData, setBooksData] = useState([]);
-
-  const handleClick = () => {
-    request
-      // superagent
-      .get("https://www.googleapis.com/books/v1/volumes")
-      .query({ q: userInput })
-      .then((data) => {
-        console.log(data);
-        setBooksData(data.body.items);
-        // this.setState({ books: [...data.body.items] });
-      });
-    console.log(booksData);
-  };
-
-=======
->>>>>>> main
   return (
     <div className="area">
       <form onSubmit={props.findBook} action="">
