a8ed605b2a0141c2de51a075aa6dd9027752925f
viviane <v.buschwallace@yahoo.com>
2021-07-01 09:55:59 +0200

    almost done with styling of book reviews

--- src/components/bookInfo components/Book-reviews.js
+++ src/components/bookInfo components/Book-reviews.js
@@ -4,34 +4,59 @@ import React from "react";
 // import DummyUsers from "../../dummyUsers.json";
 
 const BookReviews = () => {
+  // const handleSubmit(e)=>{
+  //   e.preventDefault
+  // }
+  // onClick={handleSubmit}
   return (
     <React.Fragment>
       <div className="book-reviews-container">
         <div className="user-book-review-container">
           <form className="user-book-review-form">
-            <div className="user-rating">rating input of user</div>
+            <div className="user-rating">
+              <label for="user-star-rating">
+                Choose how many stars you want to rate this book:
+              </label>
+              <select
+                name="star-rating"
+                className="user-star-rating"
+                id="user-star-rating"
+              >
+                <option value="">Please choose an option</option>
+                <option value="">0</option>
+                <option value="">0.5</option>
+                <option value="">1</option>
+                <option value="">1.5</option>
+                <option value="">2</option>
+                <option value="">2.5</option>
+                <option value="">3</option>
+                <option value="">3.5</option>
+                <option value="">4</option>
+                <option value="">4.5</option>
+                <option value="">5</option>
+              </select>
+            </div>
 
             <textarea
               className="user-book-review-input"
               placeholder="Write a review for this book"
             />
-
             <input type="submit" className="book-review-submit-button" />
           </form>
         </div>
 
         <div className="other-users-book-review-container">
           <div className="other-user-details-container">
-            <div className="other-user-profile-img-container">
-              <a href="/other-user-link">
+            <div className="other-user-name-date-container">
+              <div className="other-user-profile-img-container">
+                <div className="other-user-profile-avatar"></div>
+                {/* <a href="/other-user-link">
                 <img
                   className="other-user-profile-img"
                   alt="other user's profile picture"
                 />
-              </a>
-            </div>
-
-            <div className="other-user-name-date-container">
+              </a> */}
+              </div>
               <p className="other-user-name">Other user username</p>
               <p className="other-user-post-date">date of review</p>
             </div>
