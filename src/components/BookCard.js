a0f4719f7dfea3468c4d240b0a1a1ffb7970530f
svenWacker <s.u.wacker@outlook.de>
2021-07-01 09:40:42 +0200

    updating alina and keep it working

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-
+import Button from "react-bootstrap/Button";
 const BookCard = (props) => {
   return (
     <Card className="grid">
@@ -9,16 +9,24 @@ const BookCard = (props) => {
         <Card.Title>
           {" "}
           <h3>{props.title}</h3>
+          {/* if (string.length > 25) {
+  string = string.substring(0, 24) + "...";
+} */}
         </Card.Title>
         <Card.Text>
-          <h4>{props.author}</h4>
+          <h4>Author: {props.author}</h4>
         </Card.Text>
-        <p>{props.published}</p>
+        <p>
+          Published:{" "}
+          {props.published === "0000"
+            ? "Not available"
+            : props.published.substring(0, 4)}
+        </p>
         {/* <p>{props.description}</p>
         <p>{props.averageRating}</p> */}
+        <Button variant="info">Read More</Button>
       </Card.Body>
     </Card>
   );
 };
-
 export default BookCard;
