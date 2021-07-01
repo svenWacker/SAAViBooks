bd4a3b76f9b9a6a919684574c0c6083827be312f
Alex Albaran <albaranlex@icloud.com>
2021-07-01 13:07:23 +0200

    final commit

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,30 +3,32 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 const BookCard = (props) => {
   return (
-    <Card className="grid">
-      <Card.Img src={props.image} alt="book cover" />
-      <Card.Body className="card-container">
-        <Card.Title>
-          {" "}
-          <h3>{props.title}</h3>
-          {/* if (string.length > 25) {
+    <div className="card-wrapper">
+      <Card className="grid">
+        <Card.Img className="grid-img" src={props.image} alt="book cover" />
+        <Card.Body className="card-container">
+          <Card.Title>
+            {" "}
+            <h3>{props.title}</h3>
+            {/* if (string.length > 25) {
   string = string.substring(0, 24) + "...";
 } */}
-        </Card.Title>
-        <Card.Text>
-          <h4>Author: {props.author}</h4>
-        </Card.Text>
-        <p>
-          Published:{" "}
-          {props.published === "0000"
-            ? "Not available"
-            : props.published.substring(0, 4)}
-        </p>
-        {/* <p>{props.description}</p>
+          </Card.Title>
+          <Card.Text>
+            <h4>Author: {props.author}</h4>
+          </Card.Text>
+          <p>
+            Published:{" "}
+            {props.published === "0000"
+              ? "Not available"
+              : props.published.substring(0, 4)}
+          </p>
+          {/* <p>{props.description}</p>
         <p>{props.averageRating}</p> */}
-        <Button variant="info">Read More</Button>
-      </Card.Body>
-    </Card>
+          <Button variant="info">Read More</Button>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 export default BookCard;
